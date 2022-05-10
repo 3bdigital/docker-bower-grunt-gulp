8c0e3205150558c251c003de40837f0493ec7bae
David Matthews <david@3bweb.com>
2022-05-10 15:13:56 +0100

    Added multi-arch build script

---
+++ scripts/multi-arch-build-push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################################
+# Current images
+#################################################
+docker buildx build --push --platform linux/amd64 -t 3bdigital/bower-grunt-gulp:6 6
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/bower-grunt-gulp:8 8
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/bower-grunt-gulp:10 10
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/bower-grunt-gulp:12 12
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/bower-grunt-gulp:14 14
+docker buildx build --push --platform linux/amd64,linux/arm64 -t 3bdigital/bower-grunt-gulp:16 16
+
+#################################################
+# Retired images
+#################################################
+# docker buildx build --push --platform linux/amd64 -t 3bdigital/bower-grunt-gulp:4 4
